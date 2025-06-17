86c0d337fa4d76168c97f48cec1b656a6de28bbd
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-06-17 11:57:27 +0300

    updated storage account property to reference the resource group instead of the var

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_acc_name
-  resource_group_name      = var.resource_gr_name
+  resource_group_name      = azurerm_resource_group.example.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
